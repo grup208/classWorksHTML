9de7e151999e78a89eed58a775c857398852d361
Matthewkk01 <modus@gmail.com>
2024-03-13 16:10:51 +0400

    add js folder

---
+++ JS/to-do/index.js
@@ -0,0 +1,106 @@
+
+const form=document.querySelector('form');
+const tBody=document.querySelector('tbody');
+
+renderUsers(getUsers());
+
+form.addEventListener("submit", (e)=>{
+   e.preventDefault();
+   let formData = new FormData(form);
+   const user=Object.fromEntries(formData);
+   user.id=idGenerator();  
+   saveUser(user);
+   renderUsers(getUsers()); 
+   form.reset();   
+ });
+ 
+ function idGenerator(){
+   return Math.random().toFixed(4).slice(2);
+ }
+ 
+
+function saveUser(user){
+   const users=getUsers();
+   let index=users.findIndex((el)=>{return el.id===user.id});
+   index>=0?users[index]=userc : users.push(user);  
+   localStorage.setItem('users',JSON.stringify(users));
+}
+
+function deleteUser(id){
+   const users=getUsers();
+   let index=users.findIndex(el=>el.id===id);
+   if(index===-1) return;
+   users.splice(index,1);  
+   localStorage.setItem('users',JSON.stringify(users));
+   renderUsers(getUsers());
+}
+
+function getUsers(){
+   return JSON.parse(localStorage.getItem('users')) ?? [];
+}
+
+function setDeleteLisener(){
+   const deleteBtns=Array.from(document.querySelectorAll('.delete'));
+   deleteBtns.forEach(el=>{
+      el.addEventListener('click',(e)=>{
+         const userId=e.target.attributes.userId.value;
+         deleteUser(userId);   
+      })
+   })
+}
+
+function renderUsers(arr){
+   tBody.innerHTML='';
+   arr.forEach(user => {
+      tBody.innerHTML+=`
+      <tr>
+      <td>${user.id}</td>
+      <td>${user.fName}</td>
+      <td>${user.lName}</td>
+      <td>${user.email}</td>
+      <td>${user.phone}</td>
+      <td>${user.addres}</td>
+      <td>
+        <img
+          id="edit"
+          src="./download (1).png"
+          width="30px"
+          height="30px"
+        />
+        <img
+          userId=${user.id}
+          class="delete"
+          src="./6861362.png"
+          width="30px"
+          height="30px"
+        />
+      </td>
+    </tr>      `
+    setDeleteLisener();
+   });
+}
+
+class nameValidator{
+   isValid(value){
+      
+   }
+}
+
+function inputValidator(validator,value){
+   return validator.isValid(value)
+}
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
